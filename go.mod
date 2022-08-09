a23ff0c5e56cc613c354c4587f1623718818c524
juicesix <2358343165@qq.com>
2022-08-09 16:01:24 +0800

    结构体基础复制初始化

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-copy
+
+go 1.16
+
+require (
+	github.com/juicesix/logging v0.0.0-20220210041344-181518e55d4e // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+)
---
+++ structutils/struct_utils.go
@@ -0,0 +1,90 @@
+package structutils
+
+import (
+	"encoding/json"
+	"reflect"
+
+	"github.com/juicesix/logging"
+	"github.com/mitchellh/mapstructure"
+)
+
+func DeepFields(ifaceType reflect.Type) []reflect.StructField {
+	var fields []reflect.StructField
+	for i := 0; i < ifaceType.NumField(); i++ {
+		v := ifaceType.Field(i)
+		if v.Anonymous && v.Type.Kind() == reflect.Struct {
+			fields = append(fields, DeepFields(v.Type)...)
+		} else {
+			fields = append(fields, v)
+		}
+	}
+
+	return fields
+}
+
+// StructCopy 结构体复制 DstStructPtr:目标 SrcStructPtr 来源
+func StructCopy(DstStructPtr interface{}, SrcStructPtr interface{}) {
+	srcv := reflect.ValueOf(SrcStructPtr)
+	dstv := reflect.ValueOf(DstStructPtr)
+	srct := reflect.TypeOf(SrcStructPtr)
+	dstt := reflect.TypeOf(DstStructPtr)
+	if srct.Kind() != reflect.Ptr || dstt.Kind() != reflect.Ptr ||
+		srct.Elem().Kind() == reflect.Ptr || dstt.Elem().Kind() == reflect.Ptr {
+		panic("Fatal error:type of parameters must be Ptr of value")
+	}
+	if srcv.IsNil() || dstv.IsNil() {
+		panic("Fatal error:value of parameters should not be nil")
+	}
+	srcV := srcv.Elem()
+	dstV := dstv.Elem()
+	srcfields := DeepFields(reflect.ValueOf(SrcStructPtr).Elem().Type())
+	for _, v := range srcfields {
+		if v.Anonymous {
+			continue
+		}
+		dst := dstV.FieldByName(v.Name)
+		src := srcV.FieldByName(v.Name)
+		if !dst.IsValid() {
+			continue
+		}
+		if src.Type() == dst.Type() && dst.CanSet() {
+			dst.Set(src)
+			continue
+		}
+		if src.Kind() == reflect.Ptr && !src.IsNil() && src.Type().Elem() == dst.Type() {
+			dst.Set(src.Elem())
+			continue
+		}
+		if dst.Kind() == reflect.Ptr && dst.Type().Elem() == src.Type() {
+			dst.Set(reflect.New(src.Type()))
+			dst.Elem().Set(src)
+			continue
+		}
+	}
+	return
+}
+
+// StructJsonCopy 序列化及反序列化
+func StructJsonCopy(DstStructPtr interface{}, SrcStructPtr interface{}) {
+	srcv := reflect.ValueOf(SrcStructPtr)
+	dstv := reflect.ValueOf(DstStructPtr)
+	srct := reflect.TypeOf(SrcStructPtr)
+	dstt := reflect.TypeOf(DstStructPtr)
+	if srct.Kind() != reflect.Ptr || dstt.Kind() != reflect.Ptr ||
+		srct.Elem().Kind() == reflect.Ptr || dstt.Elem().Kind() == reflect.Ptr {
+		panic("Fatal error:type of parameters must be Ptr of value")
+	}
+	if srcv.IsNil() || dstv.IsNil() {
+		panic("Fatal error:value of parameters should not be nil")
+	}
+	aj, _ := json.Marshal(SrcStructPtr)
+	_ = json.Unmarshal(aj, DstStructPtr)
+}
+
+func MapToStruct(mapInstance, result interface{}) error {
+	if err := mapstructure.Decode(mapInstance, result); err != nil {
+		logging.Errorf("MapToStruct error")
+		return err
+	}
+	return nil
+}
